4649ec505d139484b2ba1e2153f07193dbee60cc
Joaquín Ossandon <30879716+cacosandon@users.noreply.github.com>
2025-04-10 23:43:02 -0700

    chore: first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+"use client";
+
+import UniqueId from "@tiptap-pro/extension-unique-id";
+import Image from "@tiptap/extension-image";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Image,
+      UniqueId.configure({
+        types: ["paragraph", "image"],
+        attributeName: "id",
+      }),
+    ],
+    content: `no content`,
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  return (
+    <div className="bg-white w-screen h-screen p-4">
+      <div className="flex gap-2">
+        <button
+          className="bg-black text-white p-2 rounded-md"
+          onClick={() => {
+            if (!editor) return;
+
+            const content = `
+<img data-id="a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d" src="https://placehold.co/400x400?text=Hello">
+<img data-id="9e8d7c6b-5a4f-4e3d-2c1b-0a9b8c7d6e5f" src="https://placehold.co/400x400?text=World">
+<p data-id="1933a0b8-289f-4538-9596-22bab7b728d0">A simple paragraph</p>
+`;
+
+            editor?.commands.setContent(content);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Set content (duplicated IDs and paragraph id change)
+        </button>
+
+        <button
+          className="bg-black text-white p-2 rounded-md"
+          onClick={() => {
+            if (!editor) return;
+
+            const content = `
+<p data-id="1933a0b8-289f-4538-9596-22bab7b728d0">A simple paragraph</p>
+<img data-id="a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d" src="https://placehold.co/400x400?text=Hello">
+<img data-id="9e8d7c6b-5a4f-4e3d-2c1b-0a9b8c7d6e5f" src="https://placehold.co/400x400?text=World">
+`;
+
+            editor?.commands.setContent(content);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Set content (second image ID change)
+        </button>
+      </div>
+
+      <div className="mt-4 mb-2 block text-sm text-gray-600">
+        {editor && (
+          <div>
+            <h3 className="font-medium">Editor HTML:</h3>
+            <pre className="bg-gray-100 p-2 rounded-md overflow-auto max-h-40 text-xs">
+              {editor.getHTML().replace(/<(\/?)([^>]+)>/g, "<$1$2>\n")}
+            </pre>
+          </div>
+        )}
+      </div>
+
+      <EditorContent editor={editor} />
     </div>
   );
 }
